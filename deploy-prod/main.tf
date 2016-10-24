e33602a0547233b3818d0d074812467f7cc93030
Hazim <hazim@aurorasolutions.io>
2016-10-24 16:40:16 +0500

    Add ssl cert id

--- deploy-prod/main.tf
+++ deploy-prod/main.tf
@@ -179,6 +179,7 @@ resource "aws_elb" "deployer-elb-active-ssl" {
     instance_protocol = "http"
     lb_port           = 443
     lb_protocol       = "https"
+    ssl_certificate_id = "${var.ssl_certificate_id}"
   }
 
   health_check {
