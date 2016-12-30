f5cd84bf73a46f309f252807983322e278dc46da
Muhammad Hamza Zaib <hamza@aurorasolutions.io>
2016-12-30 16:30:02 +0500

    run destroy command with sudo

--- scripts/destroy.sh
+++ scripts/destroy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ../deploy-prod
-/opt/terraform/terraform destroy -force -var-file=.terraform/deploy.tfvars \
+sudo /opt/terraform/terraform destroy -force -var-file=.terraform/deploy.tfvars \
                                         -target aws_route53_record.deployer-prod-active \
                                         -target aws_route53_record.deployer-prod-active-ssl \
                                         -target aws_route53_record.deployer-prod-test \
@@ -9,4 +9,4 @@ cd ../deploy-prod
                                         -target aws_elb.deployer-elb-test \
                                         -target aws_elb.deployer-elb-active-ssl \
                                         -target aws_elb.deployer-elb-active;
-/opt/terraform/terraform destroy -force -var-file=.terraform/deploy.tfvars
+sudo /opt/terraform/terraform destroy -force -var-file=.terraform/deploy.tfvars
